06e9b8ae2bbdecfb9a045221e56d88ba1543f0e9
acharya011978 <73109592+acharya011978@users.noreply.github.com>
2020-10-24 14:43:44 +0530

    Create PalindromeAsString.java
    Added to check a string if Palindrome

---
+++ src/main/java/com/mycompany/app/PalindromeAsString.java
@@ -0,0 +1,21 @@
+import java.util.*;   
+class PalindromeAsString
+{  
+   public static void main(String args[])  
+   {  
+      String original, reverse = "";  // Objects of String class  
+      Scanner in = new Scanner(System.in);   
+      System.out.println("Enter a string/number to check if it is a palindrome");  
+      original = in.nextLine();   
+      int length = original.length(); 
+  
+      for ( int i = length - 1; i >= 0; i-- )  
+         reverse = reverse + original.charAt(i);  
+
+      if (original.equals(reverse))  
+         System.out.println("Yes, it's a palindrome");  
+      else
+         System.out.println("Entered string is not a palindrome.");   
+   }  
+
+}  
